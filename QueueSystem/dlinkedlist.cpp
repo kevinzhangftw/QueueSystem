3117a8264cb7cbfd7ca23777cda392fd14664b06
Kai Zhang <kwz@asb9700-e06.csil.sfu.ca>
2016-01-25 15:23:01 -0800

    all files added

---
+++ QueueSystem/dlinkedlist.cpp
@@ -0,0 +1,9 @@
+//
+//  dlinkedlist.cpp
+//  QueueSystem
+//
+//  Created by Kai Zhang on 2016-01-25.
+//  Copyright © 2016 Kai Zhang. All rights reserved.
+//
+
+#include "dlinkedlist.h"
---
+++ QueueSystem/ticket.h
@@ -0,0 +1,51 @@
+//
+//  ticket.hpp
+//  QueueSystem
+//
+//  Created by Kai Zhang on 2016-01-25.
+//  Copyright © 2016 Kai Zhang. All rights reserved.
+//
+
+#ifndef ticket_hpp
+#define ticket_hpp
+
+#include <stdio.h>
+// File:        ticket.h
+// Author:      Geoffrey Tien
+// Date:        January 18, 2016
+// Description: Definition of a customer complaint ticket class for CMPT 225 assignment 2
+
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+class Ticket
+{
+private:
+    unsigned int ticketid;
+    string customername;
+    string complaint;
+    
+public:
+    // default constructor
+    Ticket();
+    
+    // parameterized constructor
+    Ticket(unsigned int tid, string cname, string cplnt);
+    
+    // MUTATORS
+    
+    // ACCESSORS
+    unsigned int GetID() const;
+    string GetCustomer() const;
+    string GetComplaint() const;
+    
+    // overloaded operators
+    // equality returns true if either ticketid matches,
+    //   or customername and complaint match
+    bool operator==(const Ticket& tick) const;
+    bool operator!=(const Ticket& tick) const;
+};
+#endif /* ticket_hpp */
