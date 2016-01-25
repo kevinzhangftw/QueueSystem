d6f2566a8f032c9aa4a769bb8658d64bbfeb97c5
Kai Zhang <kwz@asb9700-e06.local>
2016-01-25 10:25:26 -0800

    Initial Commit

---
+++ QueueSystem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QueueSystem
+//
+//  Created by Kai Zhang on 2016-01-25.
+//  Copyright © 2016 Kai Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
